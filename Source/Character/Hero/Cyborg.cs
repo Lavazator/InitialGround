15b46c9d35cfc049247377db65f8c1ae366200e1
arfitale <arfitale@gmail.com>
2024-01-09 17:48:37 +0700

    Setup logic SSM and ApplyGravity function

--- Source/Character/Hero/Cyborg.cs
+++ Source/Character/Hero/Cyborg.cs
@@ -3,4 +3,36 @@ using System;
 
 public partial class Cyborg : Hero
 {
+
+    public override void _Ready()
+    {
+        base._Ready();
+        state = HeroState.Idle;
+    }
+
+    public override void StateIdle(double delta)
+    {
+        base.StateIdle(delta);
+        animationPlayer.Play("Cyborg/Idle");
+
+        if (GetDirection() != 0) {
+            state = HeroState.Move;
+        }
+    }
+
+    public override void StateMove(double delta)
+    {
+        base.StateMove(delta);
+        Vector2 velocity = Velocity;
+        float direction = GetDirection();
+
+        velocity.X = (float) speed * direction;
+        animationPlayer.Play("Cyborg/Move");
+
+        if (direction == 0) {
+            state = HeroState.Idle;
+        }
+
+        MoveAndSlide();
+    }
 }
--- Source/Character/Hero/Hero.cs
+++ Source/Character/Hero/Hero.cs
@@ -7,37 +7,56 @@ public partial class Hero : CharacterBody2D
     [Export] public double speed = 100;
 
     protected enum HeroState {Idle, Move, Death, Jump, None};
-    private HeroState state = HeroState.None;
+    protected HeroState state = HeroState.None;
+
+    // Node
+    protected Sprite2D sprite;
+    protected AnimationPlayer animationPlayer;
 
     public override void _Ready()
     {
         base._Ready();
-        Sprite2D sprite = GetNode<Sprite2D>("Sprite");
-        AnimationPlayer animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        sprite = GetNode<Sprite2D>("Sprite");
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
+        
+        if (!IsOnFloor()) {
+            ApplyGravity(delta);
+        }
 
         switch (state) {
             case HeroState.Idle:
                 StateIdle(delta);
                 break;
             case HeroState.Move:
-                StateIdle(delta);
+                StateMove(delta);
                 break;
             case HeroState.Jump:
-                StateIdle(delta);
+                StateJump(delta);
                 break;
             case HeroState.Death:
-                StateIdle(delta);
+                StateDeath();
                 break;
         }
     }
 
     public virtual void StateIdle(double delta) {}
     public virtual void StateMove(double delta) {}
-    public virtual void StateDeath(double delta) {}
     public virtual void StateJump(double delta) {}
+    public virtual void StateDeath() {}
+
+    public float GetDirection() {
+        return Input.GetAxis("MoveLeft", "MoveRight");
+    }
+
+    protected void ApplyGravity(double delta) {
+        Vector2 velocity = Velocity;
+        velocity.Y += (float)gravity * (float)delta;
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 }
