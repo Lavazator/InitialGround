1ba00be25f51904c5fc373758998fdb4bd9d6575
Lavazator <predatorofforsaken@gmail.com>
2024-01-31 15:21:17 +0700

    Add simple UI system, add CoinUI, GUI, and Game scene as root of main game

--- Source/Game.cs
+++ Source/Game.cs
@@ -1,19 +1,16 @@
 using Godot;
 using System;
 
-public partial class Level : Node2D
+public partial class Game : Node2D
 {
-
-    protected Hero player;
-    protected Camera2D camera;
-    Label coinGUI;
+    private Hero player;
+    private Camera2D camera;
 
     public override void _Ready()
     {
         base._Ready();
         player = GodotExtension.GetChildByType<Hero>(this);
         camera = GetNode<Camera2D>("Camera");
-        coinGUI = GetNode<Label>("CoinGUI");
 
         // set up the player camera
         player.ConnectCamera(camera);
@@ -22,6 +19,5 @@ public partial class Level : Node2D
     public override void _Process(double delta)
     {
         base._Process(delta);
-        coinGUI.Text = GameData.collectedCoins.ToString();
     }
 }
